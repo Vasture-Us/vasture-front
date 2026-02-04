f7fced7e810cf5eb99492c67db4309ff09deeba8
aoiorio <130330538+aoiorio@users.noreply.github.com>
2026-02-04 21:16:29 +0900

    feat(state): create loading state with riverpod

---
+++ lib/presentation/utils/state/loading_state.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'loading_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loadingStateNotifierHash() =>
+    r'959407bae98c2f2a77c97ab47df9eb15c8ddb925';
+
+/// See also [LoadingStateNotifier].
+@ProviderFor(LoadingStateNotifier)
+final loadingStateNotifierProvider =
+    NotifierProvider<LoadingStateNotifier, bool>.internal(
+  LoadingStateNotifier.new,
+  name: r'loadingStateNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loadingStateNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoadingStateNotifier = Notifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
