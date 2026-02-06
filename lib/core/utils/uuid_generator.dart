1ee1aef1e99b28ab53d2230027cf2eccd4664e93
aoiorio <130330538+aoiorio@users.noreply.github.com>
2026-02-06 18:06:24 +0900

    chore(remove): delete some files and fix dep

--- lib/core/utils/uuid_generator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:uuid/uuid.dart';
-
-class UuidGenerator {
-  static const Uuid _uuid = Uuid();
-
-  static String generate() {
-    return _uuid.v4();
-  }
-}
--- lib/presentation/screens/introduction/introduction_screen.dart
+++ lib/presentation/screens/introduction/introduction_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:vasture/presentation/components/animation_button/bounced_animation_button.dart';
+import 'package:vasture/presentation/components/button/bounced_animation_button.dart';
 import 'package:vasture/presentation/utils/theme/app_shadows.dart';
 import 'package:vasture/presentation/utils/routes/app_router.dart';
 import '../../utils/theme/app_colors.dart';
