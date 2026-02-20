589cfb9c466ac2bfa4c11308dd53028f57e67571
aoiorio <130330538+aoiorio@users.noreply.github.com>
2026-02-20 16:25:32 +0900

    add(constant): update legal links

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -11,6 +11,10 @@ class AppConstants {
 
   static const String skyPhotosBucket = 'sky-photos';
 
+  static const String privacyPolicyUrl =
+      'https://docs.google.com/document/d/16OBOQDjyxPhA13RuxCtbkc2OfDUioJyNYeSc99m6eFc/edit?hl=ja&tab=t.0';
+  static const String termsOfServiceUrl = 'https://docs.google.com/document/d/16OBOQDjyxPhA13RuxCtbkc2OfDUioJyNYeSc99m6eFc/edit?hl=ja&tab=t.xgafvhhcetg1';
+
   static const Duration apiTimeout = Duration(seconds: 30);
 
   static const int maxImageSizeBytes = 10 * 1024 * 1024; // 10MB
